566dd471c91b9c26edb5d1fede95b1821eff16a7
Esther <estherdominion2002@gmail.com>
2025-10-17 14:19:47 +0100

    day_1

---
+++ code_book/react_1/my_react_first_app/src/component/Footer.jsx
@@ -0,0 +1,9 @@
+function footer () {
+    const year = new Date().getFullYear();
+    return(
+        <footer>
+            <p>©{year} Naija Tech Hub.All rights reserved</p>
+        </footer>
+    );
+}
+export default footer;
