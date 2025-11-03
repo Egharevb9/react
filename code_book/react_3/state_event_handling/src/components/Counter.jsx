5df83693735a2418864da1eccd80e6a0ee4de977
esther <estherdominion2002@gmail.com>
2025-11-03 16:34:50 +0100

    code3

---
+++ code_book/react_3/state_event_handling/src/components/Counter.jsx
@@ -0,0 +1,31 @@
+import React, {useState} from React;
+import React from "react";
+function Counter (){
+    const [count, setCount] = useState(0); 
+    const [Decrement, setDecrement] = useState(1); 
+    const [Reset, setReset] = useState(0); 
+
+
+    function increment() {
+        
+        setCount(prevCount => prevCount + 1);
+    }
+    function Decrement() {
+        if (count > 0 )
+        setDecrement(prevCount => prevCount -1);
+    }
+    function Reset() {
+        
+        setReset();
+    }
+    return(
+        <div>
+            <button onClick={increment}>count:{count}</button>;
+            <button onClick={Decrement}>count:{count}</button>;
+            <button onClick={Reset}>Reset:{Reset}</button>;
+        </div>
+
+    );
+
+}
+export default Counter ;
